cf7f41657acae8a225440c29abbd418fa0bcc53a
ilyakia22 <ilya.22kia@gmail.com>
2022-12-21 17:29:11 +0700

    fixes

--- views/site/index.php
+++ views/site/index.php
@@ -3,7 +3,7 @@
 use yii\helpers\Url;
 use yii\widgets\ActiveForm;
 ?>
-<?php $form = ActiveForm::begin(['action' => ['site/search']]); ?>
+<?php $form = ActiveForm::begin(['action' => ['site/search'], 'id' => 'formSearch']); ?>
 <div class="row justify-content-md-center">
     <div class="col-auto col-lg-5">
         <input class="form-control form-control-lg" type="text" placeholder="Телефон, ОГРН, ОГРНИП, ИНН" name="s" value="" id="s">
@@ -12,8 +12,23 @@ use yii\widgets\ActiveForm;
         <button type="submit" class="btn btn-primary btn-lg mb-3">Искать</button>
     </div>
     <?php ActiveForm::end(); ?>
+    <div class="alert alert-info visually-hidden" id="alertSearch">
+        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+        Происходит процесс поиска, ожидайте или нажмите кнопку <b>Искать</b>.
+    </div>
 
-    <h2>Кто звонил?</h2>
+
+    <h2>Организации</h2>
+    <div class="content-white">
+        <? foreach ($organizations as $organization) : ?>
+            <div class="org-preview">
+                <a href="<?= $organization->getUrl() ?>"><?= $organization->ogrn ?></a> <?= $organization->fullname ?>
+            </div>
+        <? endforeach; ?>
+    </div>
+    <a href="<?= Url::toRoute('organization/index'); ?>" class="button primary">Еще</a>
+
+    <h2>Последние комментарии</h2>
     <div class="content-white">
         <? foreach ($commentPhones as $commentPhone) : ?>
             <div class="who-call">
@@ -28,15 +43,7 @@ use yii\widgets\ActiveForm;
         <? endforeach; ?>
     </div>
     <a href="<?= Url::toRoute('phone/'); ?>" class="button primary">Еще</a>
-    <h2>Организации</h2>
-    <div class="content-white">
-        <? foreach ($organizations as $organization) : ?>
-            <div class="org-preview">
-                <a href="<?= $organization->getUrl() ?>"><?= $organization->ogrn ?></a> <?= $organization->fullname ?>
-            </div>
-        <? endforeach; ?>
-    </div>
-    <a href="<?= Url::toRoute('organization/index'); ?>" class="button primary">Еще</a>
+
     <h2>Форум</h2>
     <? foreach ($forums as $k => $forum) : ?>
         <article class="preview">
@@ -64,6 +71,11 @@ use yii\widgets\ActiveForm;
     <?
 
     $this->registerJs(
-        "$('#s').val(window.location.hash.substring(1));"
+        "$('#s').val(window.location.hash.substring(1));
+        if (window.location.hash.length>2){
+            $('#alertSearch').removeClass('visually-hidden');
+             window.setTimeout(function() { $('#formSearch').submit(); }, 5000);
+        }
+        "
     );
     ?>
